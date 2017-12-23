cb2cc358aff063d27028335afcf501cb0b7d51fc
César Cano <cesarcanojmz@gmail.com>
2017-12-23 14:45:02 -0600

    Initial commit

---
+++ app/src/main/java/com/gmail/cesarcanojmz/mislugares/EdicionLugarActivity.java
@@ -0,0 +1,13 @@
+package com.gmail.cesarcanojmz.mislugares;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EdicionLugarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edicion_lugar);
+    }
+}
